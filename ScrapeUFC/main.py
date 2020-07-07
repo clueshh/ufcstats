4a3ab948f8217656f25c9196b677ffaeea9b8b62
clueshh <campbell.whitworth@gmail.com>
2020-07-07 20:50:35 +1200

    change settings

--- ScrapeUFC/main.py
+++ ScrapeUFC/main.py
@@ -1,10 +1,15 @@
+import os
+from datetime import datetime
+
 from scrapy.crawler import CrawlerProcess
 
 from ScrapeUFC.spiders import UFCSpider
 
 if __name__ == '__main__':
     process = CrawlerProcess({
-        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+        'LOG_LEVEL': 'INFO',  # 'DEBUG'
+        'LOG_FILE': os.path.join('logs', datetime.now().strftime('%Y-%m-%d %H%M%S') + ' scrapy.log')
     })
 
     process.crawl(UFCSpider)
